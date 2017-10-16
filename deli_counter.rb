498c50d060d4011da4141043bec4e06140c0032f
Adam Walter <adam.iverson.walter@gmail.com>
2017-10-16 18:56:33 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -23,6 +23,13 @@ def take_a_number(katz_deli, name)
   puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
 end
 
+counter = 0
+def take_a_number(katz_deli)
+  counter += 1
+
+  katz_deli << counter
+end
+
 def now_serving(katz_deli)
   
   if katz_deli.length < 1
